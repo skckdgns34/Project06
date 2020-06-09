74e1df154cf0485623fa7127da8ad107f2959cf6
admin <admin@YD03-14>
2020-06-09 12:31:51 +0900

    hh

--- Project06/src/com/yedam/javafx/GradeController.java
+++ Project06/src/com/yedam/javafx/GradeController.java
@@ -25,34 +25,50 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class GradeController implements Initializable {
-	@FXML
-	TableView<Grade> tableView;
-	@FXML
-	Button btnAdd, btnChart;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-	ObservableList<Grade> scores;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-	Stage primaryStage;
-
-	void setPrimaryStage(Stage PrimaryStage) {
-		this.primaryStage = primaryStage;
-	}
-
+public class GradeController implements Initializable{
+	@FXML TableView<Grade> tableView;
+	@FXML Button updateBtn, cancelBtn, btn, charbtn;
+	Connection conn;
+	PreparedStatement pstmt = null;
+	ObservableList<Grade> grade;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		scores = FXCollections.observableArrayList();
+		 grade = getGradeList();
+		
+		TableColumn<Grade, ?> tcMonth = tableView.getColumns().get(0);
+		tcMonth.setCellValueFactory(new PropertyValueFactory("month"));
+		
+		TableColumn<Grade, ?> tckorean = tableView.getColumns().get(1);
+		tckorean.setCellValueFactory(new PropertyValueFactory("korean"));
+		
+		TableColumn<Grade, ?> tcMath = tableView.getColumns().get(2);
+		tcMath.setCellValueFactory(new PropertyValueFactory("english"));
+		
+		TableColumn<Grade, ?> tcEnglish = tableView.getColumns().get(3);
+		tcEnglish.setCellValueFactory(new PropertyValueFactory("math"));
+		
+		tableView.setItems(grade);
 
-		btnAdd.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent arg0) {
-				buttonAddAction(arg0);
-
-			}
-		});
-		btnChart.setOnAction(new EventHandler<ActionEvent>() {
+		
+		charbtn.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
@@ -60,65 +76,55 @@ public class GradeController implements Initializable {
 
 			}
 		});
-
-		TableColumn<Grade, ?> tcMonth = tableView.getColumns().get(0);
-		tcMonth.setCellValueFactory(new PropertyValueFactory("month"));
-		TableColumn<Grade, ?> tcKorean = tableView.getColumns().get(1);
-		tcKorean.setCellValueFactory(new PropertyValueFactory("korean"));
-		TableColumn<Grade, ?> tcMath = tableView.getColumns().get(2);
-		tcMath.setCellValueFactory(new PropertyValueFactory("math"));
-		TableColumn<Grade, ?> tcEnglish = tableView.getColumns().get(3);
-		tcEnglish.setCellValueFactory(new PropertyValueFactory("english"));
-
-		tableView.setItems(scores);
 	}
-
-	public void buttonAddAction(ActionEvent ae) {
-		Stage addStage = new Stage(StageStyle.UTILITY);
-		addStage.initModality(Modality.WINDOW_MODAL);
-		addStage.initOwner(btnAdd.getScene().getWindow());
-
+	// connect
+	public Connection getConnect() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("AddForm.fxml"));
-			Scene scene = new Scene(parent);
-			addStage.setScene(scene);
-			addStage.setResizable(false);
-			addStage.show();
-
-			Button btnFormAdd = (Button) parent.lookup("#btnFormAdd");
-			btnFormAdd.setOnAction(new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent arg0) {
-					TextField txtMonth = (TextField) parent.lookup("#txtMonth");
-					TextField txtKorean = (TextField) parent.lookup("#txtKorean");
-					TextField txtMath = (TextField) parent.lookup("#txtMath");
-					TextField txtEnglish = (TextField) parent.lookup("#txtEnglish");
-					Grade student = new Grade(txtMonth.getText(), Integer.parseInt(txtKorean.getText()),
-							Integer.parseInt(txtMath.getText()), Integer.parseInt(txtEnglish.getText()));
-					scores.add(grade);
-					addStage.close();
-				}
-			});
-
-		} catch (IOException e) {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, "hr", "hr");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		return conn;
 	}
+	
+	//성적 목록
+	public ObservableList<Grade> getGradeList() {
+		ObservableList<Grade> list = FXCollections.observableArrayList();
+		conn = getConnect();
+		String sql = "select month, korean, english, math from grade";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Grade grade = new Grade(rs.getString("month"), rs.getInt("korean"),rs.getInt("english"),rs.getInt("math"));
+				list.add(grade);
+				System.out.println(list.get(0).getEnglish());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+
 
 	public void buttonChartAction(ActionEvent ae) {
 		Stage chartStage = new Stage(StageStyle.UTILITY);
 		chartStage.initModality(Modality.WINDOW_MODAL);
-		chartStage.initOwner(btnAdd.getScene().getWindow());
+		chartStage.initOwner(btn.getScene().getWindow());
 
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("ScoreChart.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getResource("Chart.fxml"));
 			BarChart barChart = (BarChart) parent.lookup("#barChart");
 
 			XYChart.Series<String, Integer> serieskorean = new XYChart.Series<String, Integer>();
 			ObservableList<XYChart.Data<String, Integer>> dataskorean = FXCollections.observableArrayList();
-			for (int i = 0; i < scores.size(); i++) {
-				dataskorean.add(new XYChart.Data(scores.get(i).getName(), scores.get(i).getKorean()));
+			for (int i = 0; i < grade.size(); i++) {
+				dataskorean.add(new XYChart.Data(grade.get(i).getName(), grade.get(i).getKorean()));
 				// "이름",국어점수
 			}
 			serieskorean.setData(dataskorean);
@@ -126,8 +132,8 @@ public class GradeController implements Initializable {
 
 			XYChart.Series<String, Integer> seriesMath = new XYChart.Series<String, Integer>();
 			ObservableList<XYChart.Data<String, Integer>> datasMath = FXCollections.observableArrayList();
-			for (int i = 0; i < scores.size(); i++) {
-				datasMath.add(new XYChart.Data(scores.get(i).getName(), scores.get(i).getMath()));
+			for (int i = 0; i < grade.size(); i++) {
+				datasMath.add(new XYChart.Data(grade.get(i).getName(), grade.get(i).getMath()));
 				// "이름",국어점수
 			}
 			seriesMath.setData(datasMath);
@@ -136,8 +142,8 @@ public class GradeController implements Initializable {
 
 			XYChart.Series<String, Integer> seriesEnglish = new XYChart.Series<String, Integer>();
 			ObservableList<XYChart.Data<String, Integer>> datasEnglish = FXCollections.observableArrayList();
-			for (int i = 0; i < scores.size(); i++) {
-				datasEnglish.add(new XYChart.Data(scores.get(i).getName(), scores.get(i).getEnglish()));
+			for (int i = 0; i < grade.size(); i++) {
+				datasEnglish.add(new XYChart.Data(grade.get(i).getName(), grade.get(i).getEnglish()));
 				// "이름",영어점수
 			}
 			seriesEnglish.setData(datasEnglish);
--- Project06/src/com/yedam/javafx/GradeMain.java
+++ Project06/src/com/yedam/javafx/GradeMain.java
@@ -10,7 +10,7 @@ public class GradeMain extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("GradeControl.fxml"));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
