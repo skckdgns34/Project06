a85fddc7e881ddc8a836f860cb75f80b722eec3f
choich <skckdgns34@naver.com>
2020-06-11 13:46:58 +0900

    as

--- Project06/src/project/Main.java
+++ Project06/src/project/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("LoginControl.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
